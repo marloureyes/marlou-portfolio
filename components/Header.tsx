f5c2ff5e88e790b3b4247224cada2b00681a8836
Marlou Reyes <marloureyesdr@gmail.com>
2025-07-05 05:45:37 +0800

    Fix Logo

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,7 +8,12 @@ const Header = () => {
   return (
     <header className="flex items-center justify-between w-full px-20 py-16 absolute top-0 left-0 z-10 ">
       <div className="flex items-center gap-12">
-        <Image src="/logo.svg" alt="Logo" width={172} height={57} />
+        <Image
+          src="/logos/site-logo.svg"
+          alt="Lou Logo"
+          width={172}
+          height={57}
+        />
         <CtaButton />
       </div>
       <div className="flex items-center gap-10">
